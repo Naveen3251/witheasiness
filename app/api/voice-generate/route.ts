388fd322b881254da9623c9b51b5c9746512780f
Naveen Kumar <114800360+Naveen3251@users.noreply.github.com>
2024-02-26 22:41:42 +0530

    Update route.ts

--- app/api/voice-generate/route.ts
+++ app/api/voice-generate/route.ts
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 
 import { Resemble } from '@resemble/node' // If you are using es modules
 
-Resemble.setApiKey("EqNczEgN1ItqCuc6hX50rgtt")
+Resemble.setApiKey("Paste your resemble api key")
 
 interface Response {
     items: any[]; 
@@ -32,17 +32,17 @@ export async function POST(req:Request) {
          if(!user || !user.id || !user.firstName){
             return new NextResponse("Unauthorized",{status:401});
         }
-        
-        const voice_clip = await Resemble.v2.clips.createSync("7bc9ec89",{
+        project_uuid=""//place your project uuid
+        voice_uuid=""//place your voice uuid
+        const voice_clip = await Resemble.v2.clips.createSync(project_uuid,{
             body: prompt,
-            voice_uuid:"b2d1bb75",
+            voice_uuid:voice_uuid,
             is_public: false,
             is_archived: false
         });
         console.log(voice_clip)
         const audio_src=(voice_clip as WriteResponseV2<Clip>).item.audio_src;
-        //const audio_src=`https://app.resemble.ai/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBCRkhBbHcwPSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--adfb833e29531e8d97d4db1f8275ff54e58006be/result.wav`
-        console.log(audio_src)
+        
         return new NextResponse(audio_src, { status: 200 });
     }catch(error){
         return new NextResponse("An error occured",{status:404});
